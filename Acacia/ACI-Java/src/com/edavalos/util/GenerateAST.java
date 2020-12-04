fc26c43d7c24f445ad05eee3466d00ef2fe2be24
Enrique <edavalos@ucsc.edu>
2020-12-03 21:14:14 -0800

    Create GenerateAST utility
    Only the skeleton so far - this side program will autogenerate classes for each of our token types.

---
+++ Acacia/ACI-Java/src/com/edavalos/util/GenerateAST.java
@@ -0,0 +1,13 @@
+package com.edavalos.util;
+
+import java.io.IOException;
+
+public final class GenerateAST {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+    }
+}
