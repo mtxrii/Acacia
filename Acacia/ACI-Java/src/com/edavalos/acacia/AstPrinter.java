345a3cf2127f5572f860aba6c3934eb3ded1827d
Enrique <edavalos@ucsc.edu>
2020-12-22 02:22:52 -0800

    Added visitUnaryExpr() method

--- Acacia/ACI-Java/src/com/edavalos/acacia/AstPrinter.java
+++ Acacia/ACI-Java/src/com/edavalos/acacia/AstPrinter.java
@@ -26,4 +26,17 @@ public final class AstPrinter implements Expr.Visitor<String> {
     public String visitUnaryExpr(Expr.Unary expr) {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
+
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(").append(name);
+        for (Expr expr : exprs) {
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
+        builder.append(")");
+
+        return builder.toString();
+    }
 }
