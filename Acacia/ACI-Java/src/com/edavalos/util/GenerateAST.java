0c83d84724f050fbd0eda3bca0e62cea528e2d6c
Enrique <edavalos@ucsc.edu>
2020-12-21 16:04:40 -0800

    Finished class generator script

--- Acacia/ACI-Java/src/com/edavalos/util/GenerateAST.java
+++ Acacia/ACI-Java/src/com/edavalos/util/GenerateAST.java
@@ -33,7 +33,38 @@ public final class GenerateAST {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        // The AST classes.
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("  static class " + className + " extends " + baseName + " {");
+
+        // Constructor.
+        writer.println("    " + className + "(" + fieldList + ") {");
+
+        // Store parameters in fields.
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("      this." + name + " = " + name + ";");
+        }
+
+        writer.println("    }");
+
+        // Fields.
+        writer.println();
+        for (String field : fields) {
+            writer.println("    final " + field + ";");
+        }
+
+        writer.println("  }");
     }
 }
