df77878b2a4b0dd35ca46fda26e2619f9d0094ab
Enrique <edavalos@ucsc.edu>
2020-12-22 02:35:36 -0800

    Removed test main() method.
    ...It works

--- Acacia/ACI-Java/src/com/edavalos/acacia/AstPrinter.java
+++ Acacia/ACI-Java/src/com/edavalos/acacia/AstPrinter.java
@@ -39,14 +39,4 @@ public final class AstPrinter implements Expr.Visitor<String> {
 
         return builder.toString();
     }
-
-    public static void main(String[] args) {
-        Expr expression = new Expr.Binary(
-            new Expr.Unary(new Token(TokenType.MINUS, "-", null, 1),
-                    new Expr.Literal(123)),
-            new Token(TokenType.STAR, "*", null, 1),
-            new Expr.Grouping(new Expr.Literal(45.67)));
-
-        System.out.println(new AstPrinter().print(expression));
-    }
 }
