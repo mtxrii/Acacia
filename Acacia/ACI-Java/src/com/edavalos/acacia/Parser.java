fc48f782f283c262387e3cb0913e1005e0d1b26c
Enrique <edavalos@ucsc.edu>
2020-12-24 00:04:00 -0800

    Added expression and equality to parser

--- Acacia/ACI-Java/src/com/edavalos/acacia/Parser.java
+++ Acacia/ACI-Java/src/com/edavalos/acacia/Parser.java
@@ -12,6 +12,22 @@ class Parser {
         this.tokens = tokens;
     }
 
+    private Expr expression() {
+        return equality();
+    }
+
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
     private boolean match(TokenType... types) {
         for (TokenType type : types) {
             if (check(type)) {
