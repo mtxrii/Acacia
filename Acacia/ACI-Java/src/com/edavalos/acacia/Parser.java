26444e32b16a3f79f21172c81ccd9818a04fbfcb
Enrique <edavalos@ucsc.edu>
2020-12-24 00:02:00 -0800

    Created Parser.java with utilities to traverse tokens

---
+++ Acacia/ACI-Java/src/com/edavalos/acacia/Parser.java
@@ -0,0 +1,47 @@
+package com.edavalos.acacia;
+
+import java.util.List;
+
+import static com.edavalos.acacia.TokenType.*;
+
+class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+}
