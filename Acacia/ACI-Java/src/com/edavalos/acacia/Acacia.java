331c6d3e60778fd5ca20158384e19b52aa9ba959
Enrique <edavalos@ucsc.edu>
2020-12-27 04:58:22 -0800

    Unit test for parser

--- Acacia/ACI-Java/src/com/edavalos/acacia/Acacia.java
+++ Acacia/ACI-Java/src/com/edavalos/acacia/Acacia.java
@@ -68,10 +68,13 @@ public final class Acacia {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        // For now, just print the tokens
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     // For reporting errors given only a line and message
