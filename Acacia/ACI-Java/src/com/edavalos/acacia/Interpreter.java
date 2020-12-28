b460e6acc621365f5ca01fcea6dcc7ff68418ff0
Enrique <edavalos@ucsc.edu>
2020-12-28 02:13:13 -0800

    Added interpreting for binaries
    and some more helper functions

--- Acacia/ACI-Java/src/com/edavalos/acacia/Interpreter.java
+++ Acacia/ACI-Java/src/com/edavalos/acacia/Interpreter.java
@@ -6,7 +6,76 @@ class Interpreter implements Expr.Visitor<Object> {
 
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
-        return null;
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        return switch (expr.operator.type) {
+            // If minus, assume both are numbers and return difference (num)
+            case MINUS -> (double)left - (double)right;
+            // If addition, add numbers together, and concatenate strings
+            case PLUS -> {
+                if (left instanceof Double && right instanceof Double) {
+                    yield (double)left + (double)right;
+                }
+
+                if (left instanceof String || right instanceof String) {
+                    yield stringify(left) + stringify(right);
+                }
+                yield null;
+            }
+            // If division, assume both are numbers and return quotient (num)
+            case SLASH -> (double)left / (double)right;
+            // If multiplication, assume both are numbers and return product (num)
+            case STAR -> (double)left * (double)right;
+
+            // If greater/less or any variant, compare number sizes, and compare string lengths (bool for both)
+            case GREATER -> {
+                if (left instanceof Double && right instanceof Double) {
+                    yield  (double)left > (double)right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    yield ((String)left).length() > ((String)right).length();
+                }
+                yield null;
+            }
+            case GREATER_EQUAL -> {
+                if (left instanceof Double && right instanceof Double) {
+                    yield (double)left >= (double)right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    yield ((String)left).length() >= ((String)right).length();
+                }
+                yield null;
+            }
+            case LESS -> {
+                if (left instanceof Double && right instanceof Double) {
+                    yield (double)left < (double)right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    yield ((String)left).length() < ((String)right).length();
+                }
+                yield null;
+            }
+            case LESS_EQUAL -> {
+                if (left instanceof Double && right instanceof Double) {
+                    yield (double)left <= (double)right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    yield ((String)left).length() <= ((String)right).length();
+                }
+                yield null;
+            }
+
+            // If equality comparison, return equality value (bool)
+            case BANG_EQUAL -> !isEqual(left, right);
+            case EQUAL_EQUAL -> isEqual(left, right);
+
+            default -> null;
+        };
     }
 
     @Override
@@ -42,7 +111,16 @@ class Interpreter implements Expr.Visitor<Object> {
         return expr.accept(this);
     }
 
-    // Determines whether a given value is true or false
+    // Convert a value into a string
+    private String stringify(Object object) {
+        // If object is nil, string returned should be 'nil' instead of 'null'
+        if (object == null) return "nil";
+
+        // Otherwise, valueOf() should take care of it
+        return String.valueOf(object);
+    }
+
+    // Determines whether a given value is considered true
     private boolean isTruthy(Object object) {
         // Anything nil is false
         if (object == null) return false;
@@ -55,5 +133,13 @@ class Interpreter implements Expr.Visitor<Object> {
 
         // Anything else is true
         return true;
+    }
+
+    // Determined whether two values are equal
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+
+        return a.equals(b);
     }
 }
