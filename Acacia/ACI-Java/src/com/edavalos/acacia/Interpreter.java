510c1cba46b46046f170b229b592504faff00932
Enrique <edavalos@ucsc.edu>
2020-12-28 00:53:14 -0800

    Create Interpreter.java skeleton

---
+++ Acacia/ACI-Java/src/com/edavalos/acacia/Interpreter.java
@@ -0,0 +1,24 @@
+package com.edavalos.acacia;
+
+class Interpreter implements Expr.Visitor<Object> {
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        return null;
+    }
+}
