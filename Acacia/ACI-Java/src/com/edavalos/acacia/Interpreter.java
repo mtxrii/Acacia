4be04534411077c9f3ac1ad283bda0651e5a8aab
Enrique <edavalos@ucsc.edu>
2020-12-28 01:32:26 -0800

    Added some helper functions
    also improved switch statement in visitUnaryExpr

--- Acacia/ACI-Java/src/com/edavalos/acacia/Interpreter.java
+++ Acacia/ACI-Java/src/com/edavalos/acacia/Interpreter.java
@@ -2,6 +2,8 @@ package com.edavalos.acacia;
 
 class Interpreter implements Expr.Visitor<Object> {
 
+    /* --- Expressions' visitor methods --- */
+
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
         return null;
@@ -21,16 +23,37 @@ class Interpreter implements Expr.Visitor<Object> {
     public Object visitUnaryExpr(Expr.Unary expr) {
         Object right = evaluate(expr.right);
 
-        switch (expr.operator.type) {
-            // If unary is a minus, assume value is number and return its negation
-            case MINUS:
-                return -(double)right;
-            // If unary is a not, assess value as bool and return its negation
-            case BANG:
-                return !isTruthy(right);
-        }
+        return switch (expr.operator.type) {
+            // If unary is a minus, assume value is a number and return its negation
+            case MINUS -> -(double)right;
 
-        // Unreachable
-        return null;
+            // If unary is a not, assess value as bool and return its negation
+            case BANG -> !isTruthy(right);
+
+            default -> null;
+        };
+    }
+
+
+    /* --- Utility methods --- */
+
+    // Sends the given expression back into the interpreter’s visitor method
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    // Determines whether a given value is true or false
+    private boolean isTruthy(Object object) {
+        // Anything nil is false
+        if (object == null) return false;
+
+        // Any boolean is just itself
+        if (object instanceof Boolean) return (boolean)object;
+
+        // Any number is false only if its value is zero
+        if (object instanceof Double) return ((double)object != 0.0);
+
+        // Anything else is true
+        return true;
     }
 }
