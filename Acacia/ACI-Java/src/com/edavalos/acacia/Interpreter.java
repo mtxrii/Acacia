20bb6d6b452b45feef6e219f13fedd830489069b
Enrique <edavalos@ucsc.edu>
2021-01-07 08:02:00 -0800

    Modified PRINT statement to not make newline automatically

--- Acacia/ACI-Java/src/com/edavalos/acacia/Interpreter.java
+++ Acacia/ACI-Java/src/com/edavalos/acacia/Interpreter.java
@@ -144,7 +144,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Void visitPrintStmt(Stmt.Print stmt) {
         Object value = evaluate(stmt.expression);
-        System.out.println(stringify(value));
+        System.out.print(stringify(value));
         return null;
     }
 
