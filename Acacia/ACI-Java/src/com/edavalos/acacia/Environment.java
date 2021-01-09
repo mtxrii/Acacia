1f30b53f9cb2c9ab30b850e9aae4c474c545ea68
Enrique <edavalos@ucsc.edu>
2021-01-09 09:41:00 -0800

    Added define() method

--- Acacia/ACI-Java/src/com/edavalos/acacia/Environment.java
+++ Acacia/ACI-Java/src/com/edavalos/acacia/Environment.java
@@ -4,6 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Environment {
+    // HashMap that holds all identifier->value bindings
     private final Map<String, Object> variables = new HashMap<>();
 
+    // Checks if variable already exists, and throws error if if does, otherwise saves new variable binding
+    void define(String name, Object value, int line) {
+        if (variables.containsKey(name))
+            Acacia.error(line, "Variable '" + name + "' already exists.");
+
+        else variables.put(name, value);
+    }
 }
