dc092c38bfdc8150b625e6c57d81c285066f7a1a
Enrique <edavalos@ucsc.edu>
2021-01-09 09:34:00 -0800

    Added variable identifier to potential tokens

--- Acacia/ACI-Java/src/com/edavalos/acacia/Parser.java
+++ Acacia/ACI-Java/src/com/edavalos/acacia/Parser.java
@@ -140,6 +140,10 @@ class Parser {
             return new Expr.Literal(previous().literal);
         }
 
+        if (match(IDENTIFIER)) {
+            return new Expr.Variable(previous());
+        }
+
         if (match(LEFT_PAREN)) {
             Expr expr = expression();
             consume(RIGHT_PAREN, "Expect ')' after expression.");
