836401da05faa9cd6c57de1c3c0f8390ff9f86c5
Enrique <edavalos@ucsc.edu>
2021-01-12 01:04:18 -0800

    Added nesting to environment class

--- Acacia/ACI-Java/src/com/edavalos/acacia/Environment.java
+++ Acacia/ACI-Java/src/com/edavalos/acacia/Environment.java
@@ -7,18 +7,32 @@ class Environment {
     // HashMap that holds all identifier->value bindings
     private final Map<String, Object> variables = new HashMap<>();
 
+    // Environment to link global scope to various inner blocks
+    final Environment enclosing;
+
     // Looks up a variable and returns it, or throws error if it does not exist
     Object get(Token name) {
         if (variables.containsKey(name.lexeme)) {
             return variables.get(name.lexeme);
         }
 
+        // checks parent as well
+        if (enclosing != null) {
+            return enclosing.get(name);
+        }
+
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
+    // Looks up a variable and assigns a new value to it, or throws an error if it does not exist
     void assign(Token name, Object value) {
         if (variables.containsKey(name.lexeme)) {
             variables.put(name.lexeme, value);
+        }
+
+        // checks parent as well
+        else if (enclosing != null) {
+            enclosing.assign(name, value);
         }
 
         else throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
