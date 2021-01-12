89dc6d801f247886349a42a2febedeaf634b877b
Enrique <edavalos@ucsc.edu>
2021-01-12 03:00:54 -0800

    Added if statements to syntax

--- Acacia/ACI-Java/src/com/edavalos/util/GenerateAST.java
+++ Acacia/ACI-Java/src/com/edavalos/util/GenerateAST.java
@@ -28,6 +28,8 @@ public final class GenerateAST {
         List<String> stmts = Arrays.asList(
                 "Block      : List<Stmt> statements",
                 "Expression : Expr expression",
+                "If         : Expr condition, Stmt thenBranch," +
+                            " Stmt elseBranch",
                 "Print      : Expr expression",
                 "Var        : Token name, Expr initializer"
         );
